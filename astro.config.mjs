8f9e470b7708367cbe170475cc62e0954b0d51d0
Cristian Villalobos <cristianvillalobos666@gmail.com>
2025-03-05 22:32:52 -0300

    Eliminar la configuración de variables de entorno públicas en el archivo de configuración de Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,17 +13,6 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
-  env: {
-    schema: {
-      // Variables públicas (accesibles desde el cliente)
-      PUBLIC_API_URL: {
-        type: 'string',
-        context: 'client',
-        access: 'public'
-      },
-    }
-  },
-
   integrations: [react()],
   adapter: netlify()
 });
